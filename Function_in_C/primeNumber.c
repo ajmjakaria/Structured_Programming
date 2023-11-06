0e4a5df1ebb1dcf1400f213bf0a08456973e5913
Abu Jafar Md Jakaria <abujafar492@gmail.com>
2023-11-06 13:07:23 +0600

    modify main function of prime check code

--- Function_in_C/primeNumber.c
+++ Function_in_C/primeNumber.c
@@ -1,7 +1,9 @@
 ///Program to check a number is prime or not
 #include<stdio.h>
+#include<math.h>
+#include<stdbool.h>
 
-int isPrime(int n)  //Function to check prime
+bool isPrime(int n)  //Function to check prime
 {
     if(n < 2) return 0;
     int i;
@@ -17,10 +19,15 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    int ret = isPrime(n); //Call Prime
-    if(ret == 0)
+   /* Way 1:
+   if(!isPrime(n)) //1
         printf("%d is not a prime.\n", n);
     else
-        printf("%d is a prime.\n", n);
+        printf("%d is a prime.\n", n);*/
+    //Way 2
+    //(isPrime(n)) ? printf("%d is a prime.\n", n) : printf("%d is not a prime.\n", n);
+
+    //Way 2
+    printf(isPrime(n) ? "%d is a prime.\n" : "%d is not a prime.\n", n);
     return 0;
 }
